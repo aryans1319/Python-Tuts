f7b6784f38c5e31f6f758ec597d032be878b1cb3
Aryan Shaw <aryan4p130202@gmail.com>
2021-04-17 14:39:30 +0530

    Add files via upload

---
+++ 01_variable.py
@@ -0,0 +1,16 @@
+a="Aryan"
+b=30
+c=35.44
+d= True
+
+#printing the variables
+print(a)
+print(b)
+print(c)
+print(d)
+
+#printing the type of variables
+print (type(a))
+print (type(b))
+print (type(c))
+print (type(d))
